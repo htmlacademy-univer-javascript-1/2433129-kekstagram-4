b861517fe9517f2c3fe8f5b436e177b673fa328b
Keks <keks@htmlacademy.ru>
2023-10-02 19:40:07 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,26 @@
+const checkLength = (strValue, maxLength) => strValue.length <= maxLength;
+const IsPalindrome = (string) =>
+{
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  let emptyString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    emptyString += normalizedString.at(i);
+  }
+  return emptyString === normalizedString;
+};
+const getNumbersFromString = (string) => {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!isNaN(string.at(i)))  {
+      result = result.concat(string.at(i));
+    }
+  }
+  if (result.length === 0) {
+    return NaN;
+  }
+  return +result;
+};
+
+getNumbersFromString('146.15dwawf789awd');
+IsPalindrome('awa');
+checkLength('16156', 4);
