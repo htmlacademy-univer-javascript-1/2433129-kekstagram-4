4ba92278c799d047682d465b50f85c6c36180c14
ConquestSucks <exenterprise1337@gmail.com>
2023-10-19 17:48:39 +0500

    refactored

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-const usedCommentId = [];
-const sentences = [
+const photoId = {
+  MIN: 1,
+  MAX: 25
+};
+const likes = {
+  MIN: 15,
+  MAX: 200
+};
+const countOfComments = {
+  MIN: 0,
+  MAX: 30
+};
+const countOfMessages = {
+  MIN: 1,
+  MAX: 2
+};
+
+const SENTENCES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -7,58 +23,38 @@ const sentences = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-const names = [
+const NAMES = [
   'Остапа', 'Ермака', 'Радомира', 'Кузьму', 'Сильвестра', 'Генриха', 'Корнея', 'Соломона', 'Ярополка', 'Добрыню'
 ];
 
-const randomInt = (min, max) =>  Math.floor(min + Math.random() * (max + 1 - min));
+const getRandomInt = (min, max) =>  Math.floor(min + Math.random() * (max + 1 - min));
 
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
 
-const generateComment = ()  => {
-  let id = randomInt(15, 200);
-  usedCommentId.push(id);
-  const messages = [];
-  const name = names[randomInt(1, names.length - 1)];
-  const avatar = `img/avatar-{${randomInt(1, 6)}}.svg`;
-  const countOfSentences = randomInt(1, 2);
-  for (let i = 1; i <= countOfSentences; i++) {
-    messages.push(sentences[randomInt(1, names.length - 1)]);
-  }
-  if (usedCommentId.find((index) => index === id)) {
-    id = randomInt(Math.max(usedCommentId), Math.max(usedCommentId) + 1);
-  }
-
-  return {
+const getComment = (_, id)  => {
+  const comment =  {
     id: id,
-    avatar: avatar,
-    message: messages,
-    name: name
+    avatar: `img/avatar-{${getRandomInt(photoId.MIN, photoId.MAX)}}.svg`,
+    message: shuffle(SENTENCES).slice(0, getRandomInt(countOfMessages.MIN, countOfMessages.MAX)),
+    name: NAMES[getRandomInt(0, NAMES.length - 1)]
   };
+
+  return comment;
 };
 
-const generateObject = (number) => {
-  const id = number;
-  const url = `photos/{${number}}.jpg`;
-  const description = 'any info';
-  const likes = randomInt(1, 200);
-  const comments = [];
-  for (let j = 0; j <= randomInt(0, 30); j++) {
-    comments.push(generateComment());
-  }
-  return {
+const getPost = (_, id) => {
+  id++;
+  const post =  {
     id: id,
-    url: url,
-    description: description,
-    likes: likes,
-    comments: comments
+    url: `photos/{${id}}.jpg`,
+    description: `any info about id: ${id}`,
+    likes: getRandomInt(likes.MIN, likes.MAX),
+    comments: Array.from({ length: getRandomInt(countOfComments.MIN, countOfComments.MAX) }, getComment)
   };
+
+  return post;
 };
 
-const generateList = () => {
-  const objects = [];
-  for (let i = 1; i <= 25; i++) {
-    objects.push(generateObject(i));
-  }
+const generateListOfObjects = () => Array.from({ length: 25 }, getPost);
 
-  return objects;
-};
+generateListOfObjects();
