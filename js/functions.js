42ad0d861ad76f339d93f890246e5b6641db45e2
ConquestSucks <exenterprise1337@gmail.com>
2023-10-23 19:30:20 +0500

    added function which calculate possible meeting or not

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,6 @@
 const checkLength = (strValue, maxLength) => strValue.length <= maxLength;
 
-const isPalindrome = (string) =>
-{
+const isPalindrome = (string) => {
   const normalizedString = string.replaceAll(' ', '').toLowerCase();
   let emptyString = '';
   for (let i = normalizedString.length - 1; i >= 0; i--) {
@@ -14,7 +13,7 @@ const isPalindrome = (string) =>
 const getNumbersFromString = (string) => {
   let result = '';
   for (let i = 0; i < string.length; i++) {
-    if (!isNaN(string.at(i)))  {
+    if (!isNaN(string.at(i))) {
       result = result.concat(string.at(i));
     }
   }
@@ -25,6 +24,23 @@ const getNumbersFromString = (string) => {
   return +result;
 };
 
+const convertTimeToMinutes = (time) => {
+  const minutes = time.split(':');
+  return +minutes[0] * 60 + +minutes[1];
+};
+
+const isAMeetingPossible = (start, end, meet, meetTime) => {
+  const startsAt = convertTimeToMinutes(start);
+  const endsAt = convertTimeToMinutes(end);
+  const meetStartsAt = convertTimeToMinutes(meet);
+  if (meetStartsAt >= startsAt && meetStartsAt + meetTime <= endsAt) {
+    return true;
+  }
+
+  return false;
+};
+
 getNumbersFromString('146.15dwawf789awd');
 isPalindrome('awa');
 checkLength('16156', 4);
+isAMeetingPossible('8:00', '17:30', '08:00', 900);
